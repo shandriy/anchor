8a5d6d872eabb980087fb162da60690adcec3546
shandriy <entyspite@gmail.com>
2024-07-19 19:09:28 -0700

    Added Entity destruction

--- anchor.js
+++ anchor.js
@@ -4,7 +4,7 @@ var anchor = (function() {
       var listener_ = listener ? listener : "load";
       item.addEventListener(listener_, function_);
     },
-    Entity: function(x, y, z) {
+    Entity: function(x, y, z, shown) {
       if (x && y && z) {
         this.x = x;
         this.y = y;
@@ -12,18 +12,37 @@ var anchor = (function() {
       } else {
         this.z = anchor.stored.entities.length;
       };
+      if (!!shown === shown) this.shown = shown;
       this.stored.id = anchor.stored.idCount;
       anchor.stored.idCount += 1;
       anchor.stored.entities.push(this);
+      anchor.stored.entityIds.push(this.stored.id);
     },
     stored: {
       idCount: 0,
-      entities: []
+      entities: [],
+      entityIds: [],
+      isDestroyed: function(entity) {
+        if (entity.stored.destroyed) throw new Error("This Entity is destroyed");
+      }
     }
   };
   anchor.Entity.prototype.x = 0;
   anchor.Entity.prototype.y = 0;
   anchor.Entity.prototype.z = 0;
-  anchor.Entity.prototype.stored = { id: 0 };
+  anchor.Entity.prototype.shown = true;
+  anchor.Entity.prototype.stored = {
+    id: 0,
+    destroyed: false
+  };
+  anchor.Entity.prototype.destroy = function() {
+    anchor.stored.isDestroyed(this);
+    var index = anchor.stored.entityIds.indexOf(this.stored.id);
+    if (index > -1) {
+      anchor.stored.entities.splice(index, 1);
+      anchor.stored.entityIds.splice(index, 1);
+    };
+    this.stored.destroyed = true;
+  };
   return anchor;
 })();
